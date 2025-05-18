88b3dfd7f395338885ec825ca9a861d4fa52c7a7
unknown <lucasmendoncawf@gmail.com>
2025-05-18 15:23:28 -0300

    small adjust

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Canvas from './components/Canvas';
 import SideBar from './components/Sidebar';
 import { useShapesStore } from './stores/useShape';
+import DevicesImage from './assets/images/devices.png';
 
 const App = () => {
   const { shapes, updateShape } = useShapesStore();
 
   return (
     <div>
+      <div className='size-not-allowed'>
+        <img src={DevicesImage} alt='devices' width={100} height={80} />
+        <p>Screen size not allowed. Please access from a bigger screen.</p>
+      </div>
       <div className='flex'>
         <Canvas shapes={shapes} onUpdateShape={updateShape} />
         <SideBar />
